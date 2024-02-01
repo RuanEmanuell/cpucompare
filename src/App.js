a46e4c2c0250da1b601655b379a12b8643055ebe
Ruan Emanuell Alencarr <ruanvitorcom540@gmail.com>
2024-01-31 23:43:27 -0300

    (Version 1.0) Adding search input and button

--- src/App.js
+++ src/App.js
@@ -4,37 +4,51 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
-    const [dados, setDados] = useState(null);
+  const [dados, setDados] = useState(null);
+  const [inputValue, setInputValue] = useState('');
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:3001/cpulist/ryzen54600g'); 
-                const data = await response.json();
-                JSON.stringify(data);
-                setDados(data);
-            } catch (error) {
-                console.error('Erro ao buscar dados da API:', error);
-            }
-        };
+  const clickSearchCpu = async () => {
+    setDados(null);
+    let dados = await searchCpu(inputValue);
 
-        fetchData();
-    }, []);
+    setDados(dados);
+  }
 
-    return (
-        <div>
-            {dados ? (
-              <>
-                <h1>{dados['cpuName']}</h1>
-                <h3>Preço: {dados['cpuPrice']}</h3>
-                <img src={dados['cpuImage']}></img>
-                <p>Pontuação (CPU Mark): {dados['cpuScore']}</p>
-                </>
-            ) : (
-                <p>Carregando dados...</p>
-            )}
-        </div>
-    );
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+
+  return (
+    <div>
+      {dados ? (
+        <>
+          <h1>{dados['cpuName']}</h1>
+          <h3>Preço: {dados['cpuPrice']}</h3>
+          <img src={dados['cpuImage']}></img>
+          <p>Pontuação (CPU Mark): {dados['cpuScore']}</p>
+        </>
+      ) : (
+        <>
+          <p>Carregando dados...</p>
+        </>
+      )}
+      <input value={inputValue} onChange={handleInputChange}></input>
+      <button onClick={clickSearchCpu}>Pesquisar</button>
+    </div>
+  );
 };
+
+async function searchCpu(value) {
+  try {
+    const response = await fetch(`http://localhost:3001/cpulist/${value}`);
+    const data = await response.json();
+    JSON.stringify(data);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default App;
