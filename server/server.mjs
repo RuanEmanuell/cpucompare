7d207fae86e14e2f137e09a8654d41ef6c3c73f7
Ruan Emanuell Alencarr <ruanvitorcom540@gmail.com>
2024-02-01 13:43:59 -0300

    (Version 1.0) Starting preparations on the frontend

--- server/server.mjs
+++ server/server.mjs
@@ -7,20 +7,35 @@ const app = express();
 app.use(cors());
 
 const cpuList = [
-    ['ryzen75800x3d', 'i310100f', 'ryzen54600g'],
-    [
-        'https://www.kabum.com.br/produto/320796/processador-amd-ryzen-7-5800x3d-3-4ghz-4-5ghz-max-turbo-cache-100mb-am4-sem-video-100-100000651wof?gad_source=1',
-        'https://www.kabum.com.br/produto/129960/processador-intel-core-i3-10100f-3-6ghz-4-3ghz-max-boost-cache-6mb-quad-core-8-threads-lga-1200-bx8070110100f',
-        'https://www.kabum.com.br/produto/333145/processador-amd-ryzen-5-4600g-3-7ghz-4-2ghz-max-turbo-cache-11mb-am4-video-integrado-100-100000147box?gad_source=1', 'https://www.kabum.com.br/produto/333145/processador-amd-ryzen-5-4600g-3-7ghz-4-2ghz-max-turbo-cache-11mb-am4-video-integrado-100-100000147box?gad_source=1'
-    ],
-    ['https://www.cpubenchmark.net/cpu.php?id=4823&cpu=AMD+Ryzen+7+5800X3D', 
-    'https://www.cpubenchmark.net/cpu.php?cpu=Intel+Core+i3-10100F+%40+3.60GHz&id=3863',
-    'https://www.cpubenchmark.net/cpu.php?cpu=AMD+Ryzen+5+4600G&id=3807']];
+    {
+        cpu: 'Ryzen 7 5800X3d',
+        brand: 'AMD',
+        shopLink: 'https://www.kabum.com.br/produto/320796/processador-amd-ryzen-7-5800x3d-3-4ghz-4-5ghz-max-turbo-cache-100mb-am4-sem-video-100-100000651wof?gad_source=1',
+        benchmarkLink: 'https://www.cpubenchmark.net/cpu.php?id=4823&cpu=AMD+Ryzen+7+5800X3D'
+    },
+    {
+        cpu: 'Ryzen 5 4600G',
+        brand: 'AMD',
+        shopLink: 'https://www.kabum.com.br/produto/333145/processador-amd-ryzen-5-4600g-3-7ghz-4-2ghz-max-turbo-cache-11mb-am4-video-integrado-100-100000147box?gad_source=1',
+        benchmarkLink: 'https://www.cpubenchmark.net/cpu.php?cpu=AMD+Ryzen+5+4600G&id=3807'
+    }
+]
 
 async function fetchData(cpuName) {
-    let cpuIndex = cpuList[0].indexOf(cpuName);
-    let cpuLink = cpuList[1][cpuIndex];
-    let cpuTest = cpuList[2][cpuIndex];
+    let cpuIndex = -1;
+    function formatString(string){
+        let formatedString = string.toLowerCase().replaceAll(' ', '').trim();
+        return formatedString;
+    }
+    for (var i=0; i<cpuList.length; i++){
+        if(formatString(cpuList[i]['cpu']) == formatString(cpuName)){
+            cpuIndex = i;
+            break;
+        }
+    }
+
+    let cpuLink = cpuList[cpuIndex]['shopLink'];
+    let cpuTest = cpuList[cpuIndex]['benchmarkLink'];
 
     const browserPromise = puppeteer.launch();
     const browser = await browserPromise;
@@ -30,17 +45,24 @@ async function fetchData(cpuName) {
     const name = await page.$eval('#description h2', (h2) => h2.innerText);
     const price = await page.$eval('.sc-5492faee-2.ipHrwP.finalPrice', (h4) => h4.innerText);
     const image = await page.$eval('.image img', (img) => img.src);
+    const brand = cpuList[i]['brand'];
 
     page = await browser.newPage();
     await page.goto(cpuTest);
 
-    const score = await page.$eval('.right-desc span:nth-child(3)', (span) => span.innerText);
 
-    return { cpuName: name, cpuPrice: price, cpuImage: image, cpuScore: score };
+    const score = await page.$eval('.right-desc span:nth-child(3)', (span) => span.innerText);
+    
+    return { cpuName: name, cpuPrice: price, cpuImage: image, cpuScore: score, cpuBrand: brand};
 }
 
-app.get('/cpulist/i310100f', async (req, res) => {
-    const data = await fetchData('i310100f');
+app.get('/cpulist', async (req, res) => {
+    const data = cpuList;
+    res.json(data);
+});
+
+app.get('/cpulist/ryzen75800x3d', async (req, res) => {
+    const data = await fetchData('ryzen75800x3d');
     res.json(data);
 });
 
@@ -49,10 +71,6 @@ app.get('/cpulist/ryzen54600g', async (req, res) => {
     res.json(data);
 });
 
-app.get('/cpulist/ryzen75800x3d', async (req, res) => {
-    const data = await fetchData('ryzen75800x3d');
-    res.json(data);
-});
 
 const port = 3001;
 
--- src/App.js
+++ src/App.js
@@ -3,44 +3,67 @@ import './App.css';
 
 import React, { useEffect, useState } from 'react';
 
+
 const App = () => {
   const [dados, setDados] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [cpuSelectList, setCpuList] = useState([]);
 
   const clickSearchCpu = async () => {
     setDados(null);
-    let dados = await searchCpu(inputValue);
+    setLoading(true);
+    let formatedInput = formatString(inputValue);
+    let dados = await searchCpu(formatedInput);
 
     setDados(dados);
+    setLoading(false);
   }
 
-  const handleInputChange = (event) => {
+  const handleInputChange = async (event) => {
     setInputValue(event.target.value);
+    let cpus = await listCpu();
+    let formatedInput = formatString(event.target.value);
+    let inputCpuList = cpus.filter(cpu => formatString(cpu['cpu']).includes(formatedInput) 
+    || formatString(cpu['brand']).includes(formatedInput)  || (formatString(cpu['brand']) + formatString(cpu['cpu'])).includes(formatedInput));
+    setCpuList(inputCpuList);
   }
 
 
   return (
     <div>
-      {dados ? (
+      {loading ? <>
+        <p>Carregando dados...</p>
+      </>
+        : <></>}
+
+      {dados ?
         <>
           <h1>{dados['cpuName']}</h1>
           <h3>Preço: {dados['cpuPrice']}</h3>
           <img src={dados['cpuImage']}></img>
           <p>Pontuação (CPU Mark): {dados['cpuScore']}</p>
         </>
-      ) : (
-        <>
-          <p>Carregando dados...</p>
-        </>
-      )}
+        : <></>}
       <input value={inputValue} onChange={handleInputChange}></input>
+      <select onClick = {handleInputChange}>
+        {cpuSelectList.map((cpuSelect) =><option>{cpuSelect['cpu']}</option>)}
+      </select>
       <button onClick={clickSearchCpu}>Pesquisar</button>
     </div>
   );
 };
 
+async function listCpu(){
+  const response = await fetch('http://localhost:3001/cpulist');
+  const data = await response.json();
+  JSON.stringify(data);
+  return data;
+}
+
 async function searchCpu(value) {
   try {
+    console.log(`http://localhost:3001/cpulist/${value}`);
     const response = await fetch(`http://localhost:3001/cpulist/${value}`);
     const data = await response.json();
     JSON.stringify(data);
@@ -49,6 +72,11 @@ async function searchCpu(value) {
   } catch (error) {
     console.log(error);
   }
+}
+
+function formatString(string){
+  let formatedString = string.toLowerCase().replaceAll(' ', '').trim();
+  return formatedString;
 }
 
 export default App;
